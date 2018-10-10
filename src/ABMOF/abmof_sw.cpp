a148b3c60eb62d04097d850a3d1c6ae3a09cdcb9
Min Liu <wzygzlm@gmail.com>
2018-10-10 10:45:25 +0200

    Imported some functions from test.cpp of the hls. Despite these functions are useless for the abmof, it's more convenient to syncronize changes between these two files.

--- src/ABMOF/abmof_sw.cpp
+++ src/ABMOF/abmof_sw.cpp
@@ -8,6 +8,8 @@ using namespace std;
 #include "abmof.h"
 #include "abmof_hw_accel.h"
 
+#define TEST_TIMES 10
+
 static col_pix_t slicesSW[SLICES_NUMBER][SLICE_WIDTH][SLICE_HEIGHT/COMBINED_PIXELS];
 static sliceIdx_t glPLActiveSliceIdxSW = 0;
 
@@ -79,7 +81,8 @@ void colSADSumSW(pix_t in1[BLOCK_SIZE + 2 * SEARCH_DISTANCE],
 }
 
 // Set the initial value as the max integer, cannot be 0x7fff, DON'T KNOW WHY.  
-static ap_int<16> miniRetVal = 0x7fff; static ap_uint<6> minOFRet = ap_uint<6>(0xff);
+static ap_int<16> miniRetVal = 0x7fff;
+static ap_uint<6> minOFRet = ap_uint<6>(0xff);
 
 static ap_int<16> miniSumTmp[2*SEARCH_DISTANCE + 1] = {0, 0, 0, 0, 0, 0, 0};
 static ap_int<16> localSumReg[BLOCK_SIZE][2*SEARCH_DISTANCE + 1];
@@ -152,12 +155,225 @@ void miniSADSumSW(pix_t in1[BLOCK_SIZE + 2 * SEARCH_DISTANCE],
 	*OFRet = minOFRet;
 
 	std::cout << "miniSumRetSW is: " << *miniSumRet << "\t OFRetSW is: " << std::hex << *OFRet << std::endl;
-//	std::cout << std::dec;    // Restore dec mode
+	std::cout << std::dec;    // Restore dec mode
+}
+
+void testMiniSADSumWrapperSW(apIntBlockCol_t *input1, apIntBlockCol_t *input2, int16_t eventCnt, apUint15_t *miniSum, apUint6_t *OF)
+{
+	pix_t ref[BLOCK_SIZE + 2 * SEARCH_DISTANCE], tag[BLOCK_SIZE + 2 * SEARCH_DISTANCE];
+
+	apIntBlockCol_t inData1, inData2;
+	ap_int<16> miniSumSWRet;
+	ap_uint<6> OFRetSWRet;
+
+	for(int32_t i = 0; i < eventCnt; i++)
+	{
+		// Initialize the localSumReg
+		for(int idx1 = 0; idx1 < BLOCK_SIZE; idx1++)
+		{
+			for(int idx2 = 0; idx2 < BLOCK_SIZE; idx2++)
+			{
+				localSumReg[idx1][idx2] = 0;
+			}
+		}
+		miniRetVal = 0x7fff;
+		minOFRet = ap_uint<6>(0xff);
+
+		for(int shiftOffset = 0; shiftOffset < BLOCK_SIZE + 2 * SEARCH_DISTANCE; shiftOffset++)
+		{
+			cout << "current iteration index is: " << i * (BLOCK_SIZE + 2 * SEARCH_DISTANCE) + shiftOffset << endl;
+			inData1 = *input1++;
+			inData2 = *input2++;
+
+			for(int j = 0; j < BLOCK_SIZE + 2 * SEARCH_DISTANCE; j++)
+			{
+				ref[j] = pix_t(inData1.range(4*j + 3, 4*j));
+				tag[j] = pix_t(inData2.range(4*j + 3, 4*j));
+			}
+
+			miniSADSumSW(ref, tag, shiftOffset, &miniSumSWRet, &OFRetSWRet);
+		}
+
+		std::cout << "miniSumRetSW is: " << apUint15_t(miniSumSWRet) << "\t OFRetSW is: " << std::hex << OFRetSWRet << std::endl;
+		cout << dec;
+
+		miniSum[i] = apUint15_t(miniSumSWRet);
+		OF[i] = OFRetSWRet;
+	}
+}
+
+void testSingleRwslicesSW(ap_uint<8> x, ap_uint<8> y, sliceIdx_t idx, pix_t refCol[BLOCK_SIZE + 2 * SEARCH_DISTANCE], pix_t tagCol[BLOCK_SIZE + 2 * SEARCH_DISTANCE])
+{
+	writePixSW(x, y, idx);
+	readBlockColsSW(x, y, idx + 1, idx + 2, refCol, tagCol);
+	resetPixSW(x, y, idx + 3);
+}
+
+void testRwslicesSW(uint64_t * data, sliceIdx_t idx, int16_t eventCnt, apIntBlockCol_t *refData, apIntBlockCol_t *tagData)
+{
+	// Check the accumulation slice is clear or not
+	for(int32_t xAddr = 0; xAddr < SLICE_WIDTH; xAddr++)
+	{
+		for(int32_t yAddr = 0; yAddr < SLICE_HEIGHT; yAddr = yAddr + COMBINED_PIXELS)
+		{
+			if (slicesSW[idx][xAddr][yAddr/COMBINED_PIXELS] != 0)
+			{
+				cout << "Ha! I caught you, the pixel which is not clear!" << endl;
+				cout << "x is: " << xAddr << "\t y is: " << yAddr << "\t idx is: " << idx << endl;
+			}
+		}
+	}
+
+	for(int32_t i = 0; i < eventCnt; i++)
+	{
+		uint64_t tmp = *data++;
+		ap_uint<8> xWr, yWr;
+		xWr = ((tmp) >> POLARITY_X_ADDR_SHIFT) & POLARITY_X_ADDR_MASK;
+		yWr = ((tmp) >> POLARITY_Y_ADDR_SHIFT) & POLARITY_Y_ADDR_MASK;
+		bool pol  = ((tmp) >> POLARITY_SHIFT) & POLARITY_MASK;
+		int64_t ts = tmp >> 32;
+
+		writePixSW(xWr, yWr, idx);
+
+		resetPixSW(i/(PIXS_PER_COL), (i % (PIXS_PER_COL)) * COMBINED_PIXELS, (sliceIdx_t)(idx + 3));
+//		cout << "tmp is: " << hex << tmp << endl;
+//		cout << "x is: " << xWr << "\t y is: " << yWr << "\t idx is: " << idx << endl;
+
+		for(int8_t xOffSet = 0; xOffSet < BLOCK_SIZE + 2 * SEARCH_DISTANCE; xOffSet++)
+		{
+			pix_t out1[BLOCK_SIZE + 2 * SEARCH_DISTANCE];
+			pix_t out2[BLOCK_SIZE + 2 * SEARCH_DISTANCE];
+
+//				resetPix(xRd + xOffSet, yRd , (sliceIdx_t)(idx + 3));
+
+//			resetPix(xRd + xOffSet, 1 , (sliceIdx_t)(idx + 3));
+
+			readBlockColsSW(xWr + xOffSet, yWr , idx + 1, idx + 2, out1, out2);
+
+			apIntBlockCol_t refBlockCol;
+			apIntBlockCol_t tagBlockCol;
+
+			for (int8_t l = 0; l < BLOCK_SIZE + 2 * SEARCH_DISTANCE; l++)
+			{
+				refBlockCol.range(BITS_PER_PIXEL * l + BITS_PER_PIXEL - 1, BITS_PER_PIXEL * l) = out1[l];
+				tagBlockCol.range(BITS_PER_PIXEL * l + BITS_PER_PIXEL - 1, BITS_PER_PIXEL * l) = out2[l];
+			}
+
+			*refData++ = refBlockCol;
+			*tagData++ = tagBlockCol;
+		}
+	}
+
+
+	for (int16_t resetCnt = 0; resetCnt < 2048; resetCnt = resetCnt + 2)
+	{
+		resetPixSW(resetCnt/PIXS_PER_COL, (resetCnt % PIXS_PER_COL) * COMBINED_PIXELS, (sliceIdx_t)(idx + 3));
+		resetPixSW(resetCnt/PIXS_PER_COL, (resetCnt % PIXS_PER_COL + 1) * COMBINED_PIXELS, (sliceIdx_t)(idx + 3));
+	}
+}
+
+void testTempSW(uint64_t * data, sliceIdx_t idx, int16_t eventCnt, int32_t *eventSlice)
+{
+	// Check the accumulation slice is clear or not
+	for(int32_t xAddr = 0; xAddr < SLICE_WIDTH; xAddr++)
+	{
+		for(int32_t yAddr = 0; yAddr < SLICE_HEIGHT; yAddr = yAddr + COMBINED_PIXELS)
+		{
+			if (slicesSW[idx][xAddr][yAddr/COMBINED_PIXELS] != 0)
+			{
+				for(int r = 0; r < 1000; r++)
+				{
+					cout << "Ha! I caught you, the pixel which is not clear!" << endl;
+					cout << "x is: " << xAddr << "\t y is: " << yAddr << "\t idx is: " << idx << endl;
+				}
+			}
+		}
+	}
+
+	ap_int<16> miniSumSWRet;
+	ap_uint<6> OFRetSWRet;
+
+	for(int32_t i = 0; i < eventCnt; i++)
+	{
+		uint64_t tmp = *data++;
+		ap_uint<8> xWr, yWr;
+		xWr = ((tmp) >> POLARITY_X_ADDR_SHIFT) & POLARITY_X_ADDR_MASK;
+		yWr = ((tmp) >> POLARITY_Y_ADDR_SHIFT) & POLARITY_Y_ADDR_MASK;
+		bool pol  = ((tmp) >> POLARITY_SHIFT) & POLARITY_MASK;
+		int64_t ts = tmp >> 32;
+
+		writePixSW(xWr, yWr, idx);
+
+		resetPixSW(i/(PIXS_PER_COL), (i % (PIXS_PER_COL)) * COMBINED_PIXELS, (sliceIdx_t)(idx + 3));
+//		cout << "tmp is: " << hex << tmp << endl;
+//		cout << "x is: " << xWr << "\t y is: " << yWr << "\t idx is: " << idx << endl;
+
+		// Initialize the localSumReg
+		for(int idx1 = 0; idx1 < BLOCK_SIZE; idx1++)
+		{
+			for(int idx2 = 0; idx2 < BLOCK_SIZE; idx2++)
+			{
+				localSumReg[idx1][idx2] = 0;
+			}
+		}
+		miniRetVal = 0x7fff;
+		minOFRet = ap_uint<6>(0xff);
+
+		for(int8_t xOffSet = 0; xOffSet < BLOCK_SIZE + 2 * SEARCH_DISTANCE; xOffSet++)
+		{
+			pix_t out1[BLOCK_SIZE + 2 * SEARCH_DISTANCE];
+			pix_t out2[BLOCK_SIZE + 2 * SEARCH_DISTANCE];
+
+//				resetPix(xRd + xOffSet, yRd , (sliceIdx_t)(idx + 3));
+
+//			resetPix(xRd + xOffSet, 1 , (sliceIdx_t)(idx + 3));
+
+			readBlockColsSW(xWr + xOffSet, yWr , idx + 1, idx + 2, out1, out2);
+
+			apIntBlockCol_t refBlockCol;
+			apIntBlockCol_t tagBlockCol;
+
+			for (int8_t l = 0; l < BLOCK_SIZE + 2 * SEARCH_DISTANCE; l++)
+			{
+				refBlockCol.range(BITS_PER_PIXEL * l + BITS_PER_PIXEL - 1, BITS_PER_PIXEL * l) = out1[l];
+				tagBlockCol.range(BITS_PER_PIXEL * l + BITS_PER_PIXEL - 1, BITS_PER_PIXEL * l) = out2[l];
+			}
+
+			miniSADSumSW(out1, out2, xOffSet, &miniSumSWRet, &OFRetSWRet);
+			//		testMiniSADSumWrapperSW(refBlockColData, tagBlockColData, eventCnt, miniSumSW, OFRetSW);
+
+			if (refBlockCol != 0 && tagBlockCol == 0)
+			{
+				cout << "Should not stop here, it is only a debug breakpoint." << endl;
+			}
+			cout  << "refBlockColSW is: " << hex <<  refBlockCol << endl;
+			cout  << "tagBlockColSW is: " << hex <<  tagBlockCol << endl;
+//			*refData++ = refBlockCol;
+//			*tagData++ = tagBlockCol;
+		}
+
+		apUint17_t tmp1 = apUint17_t(xWr.to_int() + (yWr.to_int() << 8) + (1 << 16));
+		ap_int<9> tmp2 = miniSumSWRet.range(8, 0);
+		apUint6_t tmpOF = OFRetSWRet;
+		ap_uint<32> output = (tmp2, (tmpOF, tmp1));
+//		std :: cout << "tmp1 is "  << std::hex << tmp1 << std :: endl;
+//		std :: cout << "tmp2 is "  << std::hex << tmp2 << std :: endl;
+//		std :: cout << "output is "  << std::hex << output << std :: endl;
+//		std :: cout << "eventSlice is "  << std::hex << output.to_int() << std :: endl;
+		*eventSlice++ = output.to_int();
+	}
+
+
+	for (int16_t resetCnt = 0; resetCnt < 2048; resetCnt = resetCnt + 2)
+	{
+		resetPixSW(resetCnt/PIXS_PER_COL, (resetCnt % PIXS_PER_COL) * COMBINED_PIXELS, (sliceIdx_t)(idx + 3));
+		resetPixSW(resetCnt/PIXS_PER_COL, (resetCnt % PIXS_PER_COL + 1) * COMBINED_PIXELS, (sliceIdx_t)(idx + 3));
+	}
 }
 
 static uint16_t areaEventRegsSW[AREA_NUMBER][AREA_NUMBER];
-static uint16_t OFRetRegsSW[2 * SEARCH_DISTANCE + 1][2 * SEARCH_DISTANCE + 1];
 static uint16_t areaEventThrSW = 1000;
+static uint16_t OFRetRegsSW[2 * SEARCH_DISTANCE + 1][2 * SEARCH_DISTANCE + 1];
 
 void parseEventsSW(uint64_t * dataStream, int32_t eventsArraySize, int32_t *eventSlice)
 {
