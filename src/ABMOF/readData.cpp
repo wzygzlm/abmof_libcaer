1a1cabbfda1a26177a19a6f3c62f9f92c08749ae
Min Liu <wzygzlm@gmail.com>
2018-10-23 18:12:30 +0200

    Read the string from a txt file to a int vector successfully. The .txt file is alo uploaded.

--- src/ABMOF/readData.cpp
+++ src/ABMOF/readData.cpp
@@ -1,13 +1,22 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <sstream>
+#include <vector>
 
 int main() 
 { 
     std::ifstream file("jAER-events.txt");
     std::string str; 
+    std::vector<int> values;
+
     while (std::getline(file, str))
     {
-	std::cout << "str is : " << std::endl;        
+        std::stringstream stream(str);
+        int n;
+        while(stream >> n){
+            std::cout << n << std::endl;
+            values.push_back(n);
+        }
     }
 }
